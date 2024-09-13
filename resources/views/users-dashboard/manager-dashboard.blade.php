8fc3dfad994c77fde202fb470ece1b5f8757c5d4
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-09-13 13:11:19 +0500

    add manager-dashboard

---
+++ resources/views/users-dashboard/manager-dashboard.blade.php
@@ -0,0 +1,51 @@
+<div class="rounded-xl border mt-5 p-5 shadow-md w-9/12 bg-white">
+    <div class="flex w-full items-center justify-between border-b pb-3">
+        <div class="flex items-center space-x-3">
+            <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+            <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+        </div>
+        <div class="flex items-center space-x-8">
+            <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: 2</button>
+            <div class="text-xs text-neutral-500">2024-09-12 12:44:19</div>
+        </div>
+    </div>
+
+    <div class="mt-4 mb-4">
+        <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+        <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis
+            dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt
+            turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
+            Curabitur eget feugiat massa
+        </div>
+    </div>
+
+    <div>
+        {{__('client@company.com')}}
+    </div>
+</div>
+
+<div class="rounded-xl border mt-5 p-5 shadow-md w-9/12 bg-white">
+    <div class="flex w-full items-center justify-between border-b pb-3">
+        <div class="flex items-center space-x-3">
+            <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+            <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+        </div>
+        <div class="flex items-center space-x-8">
+            <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: 2</button>
+            <div class="text-xs text-neutral-500">2024-09-12 12:44:19</div>
+        </div>
+    </div>
+
+    <div class="mt-4 mb-4">
+        <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+        <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis
+            dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt
+            turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
+            Curabitur eget feugiat massa
+        </div>
+    </div>
+
+    <div>
+        {{__('client@company.com')}}
+    </div>
+</div>
